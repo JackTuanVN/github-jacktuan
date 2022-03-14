1e6b0e530440b381800fe8214a4c068a0a18b184
JackTuanVN <jacktuan23022004@gmail.com>
2022-03-14 19:31:21 +0700

    testing

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,8 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long 
-#define fr(i, a, b) for(long long i=a;i<=b;i++)
-#define fd(i, a, b) for(long long i=a;i>=b;i--)
+#define ll long long
+#define fr(i, a, b) for (long long i = a; i <= b; i++)
+#define fd(i, a, b) for (long long i = a; i >= b; i--)
 const ll MAXN = 1e6 + 1;
 ll a[MAXN], save[MAXN], saveLocation[MAXN];
 ll target;
@@ -11,10 +11,11 @@ ll n;
 void enter()
 {
     cin >> n;
-    fr(i, 1, n) 
+    cin >> target;
+    fr(i, 1, n)
     {
         cin >> a[i];
-        cin >> target;
+
         saveLocation[a[i]] = i;
     }
 }
@@ -27,9 +28,10 @@ void solve()
     }
     fr(i, 1, n)
     {
-        if( save[target-a[i]] == 1)
+        if (save[target - a[i]] == 1)
         {
-            cout << saveLocation[target -  a[i]] << '\t' << saveLocation[a[i]]; 
+            cout << saveLocation[a[i]] << '\t' << saveLocation[target - a[i]];
+            break;
         }
     }
 }
@@ -38,8 +40,8 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-   enter();
-   solve();
-  //cout << "hello world";
+    enter();
+    solve();
+    // cout << "hello world";
     return 0;
 }
